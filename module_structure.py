20dbfc4394510e8d7d2863648abace7d590650f0
tromex <tromersebastian@gmail.com>
2015-01-12 15:35:44 -0300

    module structure, template

---
+++ module_structure.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Module docstring
+"""
+
+# imports
+import sys
+
+# constants
+# exception classes
+# interface functions
+# classes
+# internal functions & classes
+
+def main():
+    return 0    # success
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
